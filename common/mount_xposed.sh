fc6eae63e0aaf7445cfbefa4bac90e0fc7e1719d
storm77 <alt.zu-67efd6d@yopmail.fr>
2016-07-20 17:49:02 +0000

    Add files via upload

--- common/mount_xposed.sh
+++ common/mount_xposed.sh
@@ -3,6 +3,7 @@
 IC=/cache/xposed.img
 ID=/data/xposed.img
 IMGDIR=/xposed
+APPDIR=/cache/app
 
 log_xposed() { echo $1;log -p i -t Xposed "$0: $1";}
 log_xposed_exit() { echo $1;log -p i -t Xposed "$0: $1";exit 1;}
@@ -13,9 +14,13 @@ log_xposed_exit() { echo $1;log -p i -t Xposed "$0: $1";exit 1;}
 
 #image in /cache
 if [ "$1" == "--cache" ] && [ -f $IC ]; then
-  log_xposed "move from /cache to /data"
-  mv -f $IC $ID
-  reboot
+ log_xposed "move from /cache to /data"
+ mv -f $IC $ID
+ if [ -f $APPDIR/*-1 ]; then
+  log_xposed "found /cache/app/*-1 , moving to /data/app"
+  mv -f $APPDIR/*-1 /data/app
+ fi
+ reboot
 fi
 
 #image not found in /data
@@ -29,16 +34,16 @@ fi
 #start
 mountpoint -q $IMGDIR
 if [ $? -ne 0 ]; then
-  log_xposed "init image mount failed, starting manual mount"
-  e2fsck -p $ID
-  chcon u:object_r:system_data_file:s0 $ID
-  chmod 0600 $ID
-  mount -o ro,noatime $ID $IMGDIR && log_xposed "manual loop mount success"
+ log_xposed "init image mount failed, starting manual mount"
+ e2fsck -p $ID
+ chcon u:object_r:system_data_file:s0 $ID
+ chmod 0600 $ID
+ mount -o ro,noatime $ID $IMGDIR && log_xposed "manual loop mount success"
 else
-  log_xposed "init image mount success, starting bind mount:"
-  find $IMGDIR -type f|while read file;do
-    mount -o bind $file /system/${file#$IMGDIR/}
-  done
-  setprop xposed.mount 1
+ log_xposed "init image mount success, starting bind mount:"
+ find $IMGDIR -type f|while read file;do
+  mount -o bind $file /system/${file#$IMGDIR/}
+ done
+ setprop xposed.mount 1
 fi
 
